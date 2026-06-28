13e947603e054c4b826ee045fd4b5d44b9139a6c
Cristianosandu27 <a22505051@alunos.ulht.pt>
2026-06-28 22:48:27 +0000

    Preparar projeto Django para deploy

--- avaliacao_django/settings.py
+++ avaliacao_django/settings.py
@@ -26,7 +26,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
